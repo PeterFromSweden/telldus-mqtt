de46350a0d85d9a9d80c585dc391bfc499fccf51
PeterFromSweden <peterfromswe884@gmail.com>
2024-01-02 20:00:28 +0100

    Implemented 1st sensor to MQTT conversion.

--- src/main.c
+++ src/main.c
@@ -3,19 +3,195 @@
 #include <mqtt_protocol.h>
 #include <telldus-core.h>
 
+static void telldusDeviceEvent(int deviceId, int method, const char* data, int callbackId, void* context);
+static void telldusDeviceChangeEvent(int deviceId, int changeEvent, int changeType, int callbackId, void* context);
+static void telldusRawDeviceEvent(const char* data, int controllerId, int callbackId, void* context);
+static void telldusSensorEvent(const char* protocol, const char* model, int id, int dataType, const char* value, int timestamp, int callbackId, void* context);
+static void telldusControllerEvent(int controllerId, int changeEvent, int changeType, const char* newValue, int callbackId, void* context);
+
+static struct mosquitto* mosq;
+
+static char serial[12];
+static char firmware[12];
+static int evtController;
+static int evtSensor;
+static int evtDevice;
+static int evtDeviceChange;
+
+
+/* Callback called when the client receives a CONNACK message from the broker. */
+static void on_connect(struct mosquitto* mosq, void* obj, int reason_code)
+{
+  /* Print out the connection result. mosquitto_connack_string() produces an
+   * appropriate string for MQTT v3.x clients, the equivalent for MQTT v5.0
+   * clients is mosquitto_reason_string().
+   */
+  printf("on_connect: %s\n", mosquitto_connack_string(reason_code));
+  if ( reason_code != 0 ) {
+    /* If the connection fails for any reason, we don't want to keep on
+     * retrying in this example, so disconnect. Without this, the client
+     * will attempt to reconnect. */
+    mosquitto_disconnect(mosq);
+  }
+
+  /* You may wish to set a flag here to indicate to your application that the
+   * client is now connected. */
+}
+
+
+/* Callback called when the client knows to the best of its abilities that a
+ * PUBLISH has been successfully sent. For QoS 0 this means the message has
+ * been completely written to the operating system. For QoS 1 this means we
+ * have received a PUBACK from the broker. For QoS 2 this means we have
+ * received a PUBCOMP from the broker. */
+static void on_publish(struct mosquitto* mosq, void* obj, int mid)
+{
+  printf("Message with mid %d has been published.\n", mid);
+}
+
+
+static int getTelldusController(void)
+{
+  int res = TELLSTICK_SUCCESS;
+  int id;
+  int type;
+  int available = 0;
+  char str[32];
+  int ctrlres;
+  
+  while ( !available && res == TELLSTICK_SUCCESS )
+  {
+    res = tdController(&id, &type, str, sizeof(str), &available);
+    ctrlres = tdControllerValue(id, "serial", serial, sizeof(serial));
+    ctrlres = tdControllerValue(id, "firmware", firmware, sizeof(firmware));
+  }
+  return available;
+}
+
+static void telldusDeviceEvent(int deviceId, int method, const char* data, int callbackId, void* context)
+{
+  printf("telldusDeviceEvent %i, %i, %s, %i\r\n", deviceId, method, data, callbackId);
+}
+
+static void telldusDeviceChangeEvent(int deviceId, int changeEvent, int changeType, int callbackId, void* context)
+{
+  printf("telldusDeviceChangeEvent %i, %i, %i, %i\r\n", deviceId, changeEvent, changeType, callbackId);
+}
+
+static void telldusRawDeviceEvent(const char* data, int controllerId, int callbackId, void* context)
+{
+  printf("telldusRawDeviceEvent %s, %i, %i\r\n", data, controllerId, callbackId);
+}
+
+static void telldusSensorEvent(const char* protocol, const char* model, int id, int dataType, const char* value, int timestamp, int callbackId, void* context) 
+{
+  printf("telldusSensorEvent %s, %s, %i, %i, %s, %i, %i\r\n", protocol, model, id, dataType, value, timestamp, callbackId);
+  char payload[20];
+  int temp;
+  int rc;
+
+
+  /* Print it to a string for easy human reading - payload format is highly
+   * application dependent. */
+  snprintf(payload, sizeof(payload), "%s", value);
+
+  /* Publish the message
+   * mosq - our client instance
+   * *mid = NULL - we don't want to know what the message id for this message is
+   * topic = "example/temperature" - the topic on which this message will be published
+   * payloadlen = strlen(payload) - the length of our payload in bytes
+   * payload - the actual payload
+   * qos = 2 - publish with QoS 2 for this example
+   * retain = false - do not use the retained message feature for this message
+   */
+  char topic[70];
+  snprintf(topic, sizeof(topic), "telldus/%s/%s/%s/%i/%i", serial, protocol, model, id, dataType);
+  rc = mosquitto_publish(mosq, NULL, topic, strlen(payload), payload, 0, false);
+  if ( rc != MOSQ_ERR_SUCCESS ) {
+    fprintf(stderr, "Error publishing: %s\n", mosquitto_strerror(rc));
+  }
+}
+
+static void telldusControllerEvent(int controllerId, int changeEvent, int changeType, const char* newValue, int callbackId, void* context)
+{
+  printf("telldusControllerEvent %i, %i, %i, %s, %i\r\n", controllerId, changeEvent, changeType, newValue, callbackId);
+}
+
+
 int main(int argc, char *argv[])
 {
   printf("main\r\n");
   
   // Mosquitto lib
+  /* Required before calling other mosquitto functions */
   mosquitto_lib_init();
   int major, minor, revision;
   mosquitto_lib_version(&major, &minor, &revision);
   printf("mosquitto version %d.%d.%d\r\n", major, minor, revision);
 
+  /* Create a new client instance.
+   * id = NULL -> ask the broker to generate a client id for us
+   * clean session = true -> the broker should remove old sessions when we connect
+   * obj = NULL -> we aren't passing any of our private data for callbacks
+   */
+  mosq = mosquitto_new(NULL, true, NULL);
+  if ( mosq == NULL ) {
+    fprintf(stderr, "Error: Out of memory.\n");
+    return 1;
+  }
+
+  /* Configure callbacks. This should be done before connecting ideally. */
+  mosquitto_connect_callback_set(mosq, on_connect);
+  mosquitto_publish_callback_set(mosq, on_publish);
+
+  /* Connect to test.mosquitto.org on port 1883, with a keepalive of 60 seconds.
+   * This call makes the socket connection only, it does not complete the MQTT
+   * CONNECT/CONNACK flow, you should use mosquitto_loop_start() or
+   * mosquitto_loop_forever() for processing net traffic. */
+  int rc;
+  rc = mosquitto_connect(mosq, "127.0.0.1", 1883, 60);
+  if ( rc != MOSQ_ERR_SUCCESS ) {
+    mosquitto_destroy(mosq);
+    fprintf(stderr, "Error: %s\n", mosquitto_strerror(rc));
+    return 1;
+  }
+
+  /* Run the network loop in a background thread, this call returns quickly. */
+  rc = mosquitto_loop_start(mosq);
+  if ( 0 && rc != MOSQ_ERR_SUCCESS ) {
+    mosquitto_destroy(mosq);
+    fprintf(stderr, "Error: %s\n", mosquitto_strerror(rc));
+    return 1;
+  }
+
   // Telldus-core lib
   tdInit();
-  printf("Assuming telldus-core init was Ok...\r\n");
-  
+  if ( getTelldusController() != 1 )
+  {
+    fprintf(stderr, "Error: No connected telldus device?!?\r\n");
+    return 1;
+  }
+
+  evtController = tdRegisterControllerEvent(&telldusControllerEvent, NULL);
+  evtSensor = tdRegisterSensorEvent(&telldusSensorEvent, NULL);
+  evtDevice = tdRegisterDeviceEvent(&telldusDeviceEvent, NULL);
+  evtDeviceChange = tdRegisterDeviceChangeEvent(&telldusDeviceChangeEvent, NULL);
+
+  /* At this point the client is connected to the network socket, but may not
+ * have completed CONNECT/CONNACK.
+ * It is fairly safe to start queuing messages at this point, but if you
+ * want to be really sure you should wait until after a successful call to
+ * the connect callback.
+ * In this case we know it is 1 second before we start publishing.
+ */
+  while ( 1 )
+  {
+    // Events!
+    //publish_sensor_data(mosq);
+
+  }
+
+  mosquitto_lib_cleanup();
+
   return 0;
 }
